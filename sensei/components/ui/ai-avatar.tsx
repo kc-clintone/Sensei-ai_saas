678f35d4e1b9ce94491c484364c35071389992e9
kc-clintone <kayseeclintone@gmail.com>
2025-01-11 17:07:01 +0300

    Styling the chat page

---
+++ sensei/components/ui/ai-avatar.tsx
@@ -0,0 +1,9 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+export const AiAvatar = () => {
+  return (
+    <Avatar className="h-8 w-8">
+      <AvatarImage src="/sensei-logo.png" className="p-1"/>
+    </Avatar>
+  )
+}
